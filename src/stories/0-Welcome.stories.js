211cd4039dffc8a81fe8251ef3127c3bab9aaec6
779g <adrian.villa@779.mx>
2020-08-22 20:07:52 +0000

    Fixed compile

--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -3,7 +3,7 @@ import { linkTo } from '@storybook/addon-links'
 import { Welcome } from '@storybook/react/demo'
 
 export default {
-  title    :'Welcome',
+  title    :'ds-form',
   component:Welcome,
 }
 
