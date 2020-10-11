9b9ada73c41d9ec76131db6a83f8c078ea6c7fbc
779g <adrian.villa@779.mx>
2020-10-11 13:14:24 +0000

    Fix error lag

--- src/ui/utils/comparisonFunction.js
+++ src/ui/utils/comparisonFunction.js
@@ -1,5 +1,6 @@
 export default (p, n)  => {
   if(p.value != n.value) return false
+  if(p.errors != n.errors) return false
   //if(p.onChange != n.onChange) return false
   //if(p.setInputValue != n.setInputValue) return false
   /*
